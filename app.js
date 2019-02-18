3a413dc8bc318bbf2359d671afd336c4f2bf5085
Bryan Leanoe <bryan.leano@gmail.com>
2019-02-18 01:16:12 -0700

    add API functions

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+const personalToken = 'CrLIGIURwChO71pi';
+
 function getUserData(user) {
   axios.get(`https://lichess.org/api/user/${user}`)
   .then(function (response) {
-      console.log(response);
+    console.log(response);
   })
   .catch(function (error) {
-   
+    alert('error');
   });
 }
 
-getUserData('breaker90');
+//getUserData('breaker90');
+
+
+function getUserGame() {
+  axios.get('/api/account/playing', {
+    baseURL: 'https://lichess.org/',
+    headers: { 'Authorization': 'Bearer ' + personalToken }})
+  .then(function (res) {
+    console.log(res.data.nowPlaying[0].gameId);
+  })
+  .catch(function (error) {
+    alert(error);
+  })
+}
+
+getUserGame();
+
+/*
+function getLastBulletGame(user) {
+  axios.get(`https://lichess.org/api/games/user/${user}?perfType=bullet&max=1`)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+
+  });
+}
+
+getLastBulletGame('breaker90');
+*/
